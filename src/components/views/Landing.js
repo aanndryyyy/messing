f2def63d97738ad7da4bbdb99bd588a087da5953
Andry Pedak <andry.pedak@gmail.com>
2018-01-09 23:30:38 +0200

    Landing view component

--- src/components/views/Landing.js
+++ src/components/views/Landing.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 
+import Section from '../layout/Section';
+
 export default class Landing extends Component {
 	render() {
 		return (
-      <div>
-        Landing
-      </div>
+      <main>
+        <Section>
+          <h1>Colt Premium Series</h1>
+        </Section>
+
+        <Section>
+          <h1>Colt Series</h1>
+        </Section>
+
+        <Section>
+          <h1>Soviet Series</h1>
+        </Section>
+      </main>
 		);
 	}
 }
