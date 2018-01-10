63c51b1dc09805924dabd3fad51db77cfd97e7b5
Andry Pedak <andry.pedak@gmail.com>
2018-01-10 22:34:15 +0200

    Use layout components

--- src/components/views/Landing.js
+++ src/components/views/Landing.js
@@ -1,21 +1,34 @@
 import React, {Component} from 'react';
 
 import Section from '../layout/Section';
+import Container from '../layout/Container';
 
 export default class Landing extends Component {
 	render() {
 		return (
       <main>
         <Section>
-          <h1>Colt Premium Series</h1>
+          <Container>
+            <h1>Colt Premium Series</h1>
+          </Container>
+        </Section>
+
+        <Section class="inverted">
+          <Container>
+            <h1>Colt Series</h1>
+          </Container>
         </Section>
 
         <Section>
-          <h1>Colt Series</h1>
+          <Container>
+            <h1>Soviet Series</h1>
+          </Container>
         </Section>
 
         <Section>
-          <h1>Soviet Series</h1>
+          <Container>
+            <h1>Telli</h1>
+          </Container>
         </Section>
       </main>
 		);
