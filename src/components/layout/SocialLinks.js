04f11c38871eb5fe253e1d819896e0a61a963b70
Andry Pedak <andry.pedak@gmail.com>
2018-01-13 19:55:12 +0200

    Layout component: SocialLinks

---
+++ src/components/layout/SocialLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/SocialLinks.css';
+
+const SocialLinks = () => {
+  return (
+    <ul className="social-links">
+      <li><a href="https://www.facebook.com/ofmessing">Facebook</a></li>
+      <li><a href="https://www.instagram.com/messingof">Instagram</a></li>
+    </ul>
+  );
+}
+
+export default SocialLinks;
