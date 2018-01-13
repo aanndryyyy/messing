7ab7fe8bca78ad98ef7b9f30d1917a74d7994187
Andry Pedak <andry.pedak@gmail.com>
2018-01-13 14:14:30 +0200

    Added Messing badge to website

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import registerServiceWorker from './lib/registerServiceWorker';
 
 import Landing from './components/views/Landing';
 
+import badge from './img/messing_badge.svg';
 import './styles/index.css';
 
 ReactDOM.render(
-  <Landing />,
+  <div>
+    <Landing />
+    
+    <img className="static-badge" src={badge} alt="Messing Badge" styles />
+  </div>,
   document.getElementById('root')
 );
 
