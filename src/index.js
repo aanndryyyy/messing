1da1c24cfefc0314112addad8d480f34b47d7258
Andry Pedak <andry.pedak@gmail.com>
2018-01-30 21:18:12 +0200

    Resolve style ordering & warnings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './lib/registerServiceWorker';
@@ -5,13 +7,12 @@ import registerServiceWorker from './lib/registerServiceWorker';
 import Landing from './components/views/Landing';
 
 import badge from './img/messing_badge.svg';
-import './styles/index.css';
 
 ReactDOM.render(
   <div>
     <Landing />
     
-    <img className="static-badge" src={badge} alt="Messing Badge" styles />
+    <img className="static-badge" src={badge} alt="Messing Badge" />
   </div>,
   document.getElementById('root')
 );
